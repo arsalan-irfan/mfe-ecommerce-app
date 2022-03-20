3302a1e57a63cb2c897ede7567af757bfe7e037d
unknown <mohdarsalan318@gmail.com>
2022-03-20 19:57:52 +0500

    Initial commit

---
+++ home/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+
+import "./index.scss";
+
+const App = () => (
+  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">Home Page Content</div>
+    <Footer />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ pdp/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "home/Header";
+import Footer from "home/Footer";
+
+import "./index.scss";
+
+const App = () => (
+  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">Pdp Page</div>
+    <Footer />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
